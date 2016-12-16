8cba2fef7bf5e46949602bf2ecf1e3bbe0e4abbf
SquidDev <bonzoweb@hotmail.co.uk>
2016-12-16 22:18:47 +0000

    Convert recipes to Lua

---
+++ recipe_convert.lua
@@ -0,0 +1,136 @@
+-- Decoding
+local whites = {['\n']=true; ['\r']=true; ['\t']=true; [' ']=true; [',']=true; [':']=true}
+local controls = {["\n"]="\\n", ["\r"]="\\r", ["\t"]="\\t", ["\b"]="\\b", ["\f"]="\\f", ["\""]="\\\"", ["\\"]="\\\\"}
+
+local function removeWhite(str)
+	while whites[str:sub(1, 1)] do
+		str = str:sub(2)
+	end
+	return str
+end
+
+local decodeControls = {}
+for k,v in pairs(controls) do
+	decodeControls[v] = k
+end
+
+local function parseBoolean(str)
+	if str:sub(1, 4) == "true" then
+		return true, removeWhite(str:sub(5))
+	else
+		return false, removeWhite(str:sub(6))
+	end
+end
+
+local function parseNull(str)
+	return nil, removeWhite(str:sub(5))
+end
+
+local numChars = {['e']=true; ['E']=true; ['+']=true; ['-']=true; ['.']=true}
+local function parseNumber(str)
+	local i = 1
+	while numChars[str:sub(i, i)] or tonumber(str:sub(i, i)) do
+		i = i + 1
+	end
+	local val = tonumber(str:sub(1, i - 1))
+	str = removeWhite(str:sub(i))
+	return val, str
+end
+
+local function parseString(str)
+	str = str:sub(2)
+	local s = ""
+	while str:sub(1,1) ~= "\"" do
+		local next = str:sub(1,1)
+		str = str:sub(2)
+		assert(next ~= "\n", "Unclosed string")
+
+		if next == "\\" then
+			local escape = str:sub(1,1)
+			str = str:sub(2)
+
+			next = assert(decodeControls[next..escape], "Invalid escape character")
+		end
+
+		s = s .. next
+	end
+	return s, removeWhite(str:sub(2))
+end
+
+local parseValue
+local function parseArray(str)
+	str = removeWhite(str:sub(2))
+
+	local val = {}
+	local i = 1
+	while str:sub(1, 1) ~= "]" do
+		local v = nil
+		v, str = parseValue(str)
+		val[i] = v
+		i = i + 1
+		str = removeWhite(str)
+	end
+	str = removeWhite(str:sub(2))
+	return val, str
+end
+
+local function parseMember(str)
+	local k = nil
+	k, str = parseValue(str)
+	local val = nil
+	val, str = parseValue(str)
+	return k, val, str
+end
+
+local function parseObject(str)
+	str = removeWhite(str:sub(2))
+
+	local val = {}
+	while str:sub(1, 1) ~= "}" do
+		local k, v = nil, nil
+		k, v, str = parseMember(str)
+		val[k] = v
+		str = removeWhite(str)
+	end
+	str = removeWhite(str:sub(2))
+	return val, str
+end
+
+function parseValue(str)
+	local fchar = str:sub(1, 1)
+	if fchar == "{" then
+		return parseObject(str)
+	elseif fchar == "[" then
+		return parseArray(str)
+	elseif tonumber(fchar) ~= nil or numChars[fchar] then
+		return parseNumber(str)
+	elseif str:sub(1, 4) == "true" or str:sub(1, 5) == "false" then
+		return parseBoolean(str)
+	elseif fchar == "\"" then
+		return parseString(str)
+	elseif str:sub(1, 4) == "null" then
+		return parseNull(str)
+	end
+	return nil
+end
+
+local function decode(str)
+	str = removeWhite(str)
+	return parseValue(str)
+end
+
+print("Loading file")
+local handle = io.open("recipe_dump.json", "r")
+local data = handle:read("*a")
+handle:close()
+
+print("Decoding")
+local decoded = decode(data)
+local serialise = dofile("artist/serialise.lua")
+
+local converted = serialise.serialise(decoded)
+
+print("Writing")
+local handle = io.open("recipe_dump.lua", "w")
+handle:write(converted)
+handle:close()
