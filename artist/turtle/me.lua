5473ff12078bb33e80da8e8c66eb4a036794cd20
SquidDev <bonzoweb@hotmail.co.uk>
2018-07-07 22:03:23 +0100

    Run turtle finding in parallel
    Closes #8

--- artist/turtle/me.lua
+++ artist/turtle/me.lua
@@ -10,8 +10,9 @@ for i = 1, #peripherals do
   end
 end
 
--- Now try to find a turtle which exists remotely but not locally.
+--- Now try to find a turtle which exists remotely but not locally.
 local turtle_peripherals, turtle_targets = {}, {}
+local queue = {}
 for i = 1, #peripherals do
   local name = peripherals[i]
   local wrapped = peripheral.wrap(name)
@@ -19,12 +20,17 @@ for i = 1, #peripherals do
     -- If it's a turtle, track it as a peripheral
     turtle_peripherals[name] = true
   elseif wrapped.getTransferLocations then
-    for _, location in ipairs(wrapped.getTransferLocations()) do
-      -- If it's a turtle, then add it as a location
-      if location:find("^turtle_") then turtle_targets[location] = true end
+    queue[#queue + 1] = function()
+      for _, location in ipairs(wrapped.getTransferLocations()) do
+        -- If it's a turtle, then add it as a location
+        if location:find("^turtle_") then turtle_targets[location] = true end
+      end
     end
   end
 end
+
+-- We run .getTransferLocations in parallel as otherwise it's stupidly slow
+parallel.waitForAll(table.unpack(queue))
 
 for k, _ in pairs(turtle_peripherals) do turtle_targets[k] = nil end
 
