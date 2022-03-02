e9273ffccba2baf9b6b5e5e5bf6d15f51054c2f2
Jonathan Coates <git@squiddev.cc>
2022-03-02 19:47:45 +0000

    Some improvements to logging
     - Include a datetime rather than just offset since start. Makes it much
       easier to ponder for long-running sessions.
     - Fix log rotation failing when the .old file exists already.

--- artist/lib/log.lua
+++ artist/lib/log.lua
@@ -3,14 +3,17 @@
 -- The file is cleared when logging first starts. Each line contains
 -- the time since logging started, followed by the log message
 
-local i = 0
+local expect = require "cc.expect".expect
 
 return function(path)
+  expect(1, path, "string")
+
   if fs.exists(path) then fs.delete(path) end
 
-  local start = os.epoch("utc")
-  return function(msg)
-    local now = os.epoch("utc")
+  local i = 0
+  return function(prefix, msg)
+    expect(1, prefix, "string")
+    expect(2, msg, "string")
 
     if i > 1024 then
       -- Log files end up taking a /lot/ of space. We rotate them every 1024
@@ -19,11 +22,16 @@ return function(path)
       -- There's an argument that logging should be off by default or something,
       -- but it's so useful when it goes wrong.
       i = 0
+      fs.delete(path .. ".old")
       fs.move(path, path .. ".old")
     end
 
+    local now  = os.epoch("utc")
+    local date = os.date("%Y-%m-%d %H:%M:%S", now / 1000)
+    local ms = ("%.2f"):format((now % 1000) * 1e-3):sub(2)
+
     local handle = fs.open(path, "a")
-    handle.writeLine(("[%.2f] %s"):format((now - start) * 1e-3, msg))
+    handle.writeLine(("[%s%s] %s: %s"):format(date, ms, prefix, msg))
     handle.close()
     i = i + 1
   end
